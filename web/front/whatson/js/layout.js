a525cdba0c4f2040bd4c0d0c0d808eeb9741545d
Athenais <athenais@whatson-web.com>
2016-05-03 12:10:38 +0200

    reset structure / css

---
+++ web/front/whatson/js/layout.js
@@ -0,0 +1,79 @@
+var fixePaddingBody = function() {
+
+    $('body').css('padding-top', $('header').height() + 'px');
+}
+
+var equilibre = function (selecteur) {
+
+    var hauteurMinimum = 0;
+
+    $(this).css('min-height', '0px');
+
+
+    $(selecteur).each(function () {
+
+        var hauteur = $(this).height() + parseInt($(this).css('padding-top')) + parseInt($(this).css('padding-bottom'));
+
+        if (hauteur > hauteurMinimum) {
+            hauteurMinimum = hauteur;
+        }
+
+    });
+
+
+    $(selecteur).each(function () {
+
+        $(this).css('min-height', hauteurMinimum + 'px');
+    });
+
+}
+
+
+var equilibreLineHeight = function (selecteur) {
+
+    var hauteurMinimum = 0;
+
+
+    $(selecteur).each(function () {
+
+        var hauteur = $(this).height() + parseInt($(this).css('padding-top')) + parseInt($(this).css('padding-bottom'));
+
+        if (hauteur > hauteurMinimum) {
+            hauteurMinimum = hauteur;
+        }
+
+    });
+
+
+    $(selecteur).each(function () {
+
+        $(this).css('line-height', hauteurMinimum + 'px');
+
+    });
+
+}
+
+
+$('a[data-anchor]').click(function () {
+
+    $('html,body').animate({scrollTop: $('#' + $(this).data('anchor')).offset().top}, 'slow');
+
+    return false;
+});
+
+
+
+$(window).ready(function () {
+    fixePaddingBody();
+
+});
+
+$(window).load(function () {
+    fixePaddingBody();
+
+});
+
+$(window).resize(function () {
+    fixePaddingBody();
+
+});
