7b73a79d33aff6f650d2c50aed12ba2e76410301
Jérôme Lebleu <jerome@whatson-web.com>
2016-07-27 06:37:54 +0200

    no message

--- src/APP/BlogBundle/APPBlogBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace APP\BlogBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class APPBlogBundle extends Bundle
-{
-}
--- src/APP/CmsBundle/APPCmsBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace APP\CmsBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class APPCmsBundle extends Bundle
-{
-}
